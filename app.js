65492ca382ea8d1b1cd6869b62214abe19385b1b
Qasem <qasem@MacBook-Pro.local>
2018-12-30 20:31:30 +0300

    First commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 
     const app = express();
     app.use(cors());
-    app.use( express.static(path.join(__dirname, 'public')))
+    app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
     const connection = mysql.createConnection({
